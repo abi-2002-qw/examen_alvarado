0ac9447e3a1740903d0a0d17f2955e724cae25ef
Abigail Alvarado <abi.alvarado2210@gmail.com>
2025-03-06 16:43:08 -0500

    examen_corregido

---
+++ examen_alvarado/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace examen_alvarado
+{
+    partial class form
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dniBox = new System.Windows.Forms.TextBox();
+            this.apellidoBox = new System.Windows.Forms.TextBox();
+            this.nombreBox = new System.Windows.Forms.TextBox();
+            this.dniLabel = new System.Windows.Forms.Label();
+            this.apellidoLabel = new System.Windows.Forms.Label();
+            this.nombreLabel3 = new System.Windows.Forms.Label();
+            this.notaLabel = new System.Windows.Forms.Label();
+            this.agregarButton = new System.Windows.Forms.Button();
+            this.todosButton = new System.Windows.Forms.Button();
+            this.suspensosButton = new System.Windows.Forms.Button();
+            this.aprobadosButton = new System.Windows.Forms.Button();
+            this.mhButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.notaBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.eliminarButton = new System.Windows.Forms.Button();
+            this.consultarButton = new System.Windows.Forms.Button();
+            this.modinotaButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dniBox
+            // 
+            this.dniBox.Location = new System.Drawing.Point(185, 36);
+            this.dniBox.Name = "dniBox";
+            this.dniBox.Size = new System.Drawing.Size(169, 20);
+            this.dniBox.TabIndex = 0;
+            this.dniBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // apellidoBox
+            // 
+            this.apellidoBox.Location = new System.Drawing.Point(456, 36);
+            this.apellidoBox.Name = "apellidoBox";
+            this.apellidoBox.Size = new System.Drawing.Size(169, 20);
+            this.apellidoBox.TabIndex = 1;
+            this.apellidoBox.TextChanged += new System.EventHandler(this.apellidoBox_TextChanged);
+            // 
+            // nombreBox
+            // 
+            this.nombreBox.Location = new System.Drawing.Point(185, 71);
+            this.nombreBox.Name = "nombreBox";
+            this.nombreBox.Size = new System.Drawing.Size(169, 20);
+            this.nombreBox.TabIndex = 2;
+            this.nombreBox.TextChanged += new System.EventHandler(this.nombreBox_TextChanged);
+            // 
+            // dniLabel
+            // 
+            this.dniLabel.AutoSize = true;
+            this.dniLabel.Location = new System.Drawing.Point(100, 45);
+            this.dniLabel.Name = "dniLabel";
+            this.dniLabel.Size = new System.Drawing.Size(26, 13);
+            this.dniLabel.TabIndex = 4;
+            this.dniLabel.Text = "DNI";
+            // 
+            // apellidoLabel
+            // 
+            this.apellidoLabel.AutoSize = true;
+            this.apellidoLabel.Location = new System.Drawing.Point(371, 43);
+            this.apellidoLabel.Name = "apellidoLabel";
+            this.apellidoLabel.Size = new System.Drawing.Size(66, 13);
+            this.apellidoLabel.TabIndex = 5;
+            this.apellidoLabel.Text = "APELLIDOS";
+            // 
+            // nombreLabel3
+            // 
+            this.nombreLabel3.AutoSize = true;
+            this.nombreLabel3.Location = new System.Drawing.Point(100, 78);
+            this.nombreLabel3.Name = "nombreLabel3";
+            this.nombreLabel3.Size = new System.Drawing.Size(54, 13);
+            this.nombreLabel3.TabIndex = 6;
+            this.nombreLabel3.Text = "NOMBRE";
+            // 
+            // notaLabel
+            // 
+            this.notaLabel.AutoSize = true;
+            this.notaLabel.Location = new System.Drawing.Point(371, 78);
+            this.notaLabel.Name = "notaLabel";
+            this.notaLabel.Size = new System.Drawing.Size(37, 13);
+            this.notaLabel.TabIndex = 7;
+            this.notaLabel.Text = "NOTA";
+            // 
+            // agregarButton
+            // 
+            this.agregarButton.Location = new System.Drawing.Point(45, 162);
+            this.agregarButton.Name = "agregarButton";
+            this.agregarButton.Size = new System.Drawing.Size(148, 23);
+            this.agregarButton.TabIndex = 8;
+            this.agregarButton.Text = "AGREGAR";
+            this.agregarButton.UseVisualStyleBackColor = true;
+            this.agregarButton.Click += new System.EventHandler(this.agregarButton_Click);
+            // 
+            // todosButton
+            // 
+            this.todosButton.Location = new System.Drawing.Point(45, 191);
+            this.todosButton.Name = "todosButton";
+            this.todosButton.Size = new System.Drawing.Size(148, 23);
+            this.todosButton.TabIndex = 9;
+            this.todosButton.Text = "MOSTRAR TODOS ";
+            this.todosButton.UseVisualStyleBackColor = true;
+            this.todosButton.Click += new System.EventHandler(this.todosButton_Click);
+            // 
+            // suspensosButton
+            // 
+            this.suspensosButton.Location = new System.Drawing.Point(45, 220);
+            this.suspensosButton.Name = "suspensosButton";
+            this.suspensosButton.Size = new System.Drawing.Size(148, 23);
+            this.suspensosButton.TabIndex = 10;
+            this.suspensosButton.Text = "MOSTRAR SUSPENSOS";
+            this.suspensosButton.UseVisualStyleBackColor = true;
+            this.suspensosButton.Click += new System.EventHandler(this.suspensosButton_Click);
+            // 
+            // aprobadosButton
+            // 
+            this.aprobadosButton.Location = new System.Drawing.Point(45, 249);
+            this.aprobadosButton.Name = "aprobadosButton";
+            this.aprobadosButton.Size = new System.Drawing.Size(148, 23);
+            this.aprobadosButton.TabIndex = 11;
+            this.aprobadosButton.Text = "MOSTRAR APROBADOS";
+            this.aprobadosButton.UseVisualStyleBackColor = true;
+            this.aprobadosButton.Click += new System.EventHandler(this.aprobadosButton_Click);
+            // 
+            // mhButton
+            // 
+            this.mhButton.Location = new System.Drawing.Point(45, 278);
+            this.mhButton.Name = "mhButton";
+            this.mhButton.Size = new System.Drawing.Size(148, 23);
+            this.mhButton.TabIndex = 12;
+            this.mhButton.Text = "MOSTRAR MH";
+            this.mhButton.UseVisualStyleBackColor = true;
+            this.mhButton.Click += new System.EventHandler(this.mhButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(213, 165);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(484, 134);
+            this.listBox1.TabIndex = 15;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // notaBox
+            // 
+            this.notaBox.Location = new System.Drawing.Point(456, 71);
+            this.notaBox.Name = "notaBox";
+            this.notaBox.Size = new System.Drawing.Size(55, 20);
+            this.notaBox.TabIndex = 3;
+            this.notaBox.TextChanged += new System.EventHandler(this.notaBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(317, 378);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "ABI ALVARADO EXAMEN";
+            // 
+            // eliminarButton
+            // 
+            this.eliminarButton.Location = new System.Drawing.Point(229, 118);
+            this.eliminarButton.Name = "eliminarButton";
+            this.eliminarButton.Size = new System.Drawing.Size(75, 23);
+            this.eliminarButton.TabIndex = 19;
+            this.eliminarButton.Text = "ELIMINAR";
+            this.eliminarButton.UseVisualStyleBackColor = true;
+            this.eliminarButton.Click += new System.EventHandler(this.eliminarButton_Click);
+            // 
+            // consultarButton
+            // 
+            this.consultarButton.Location = new System.Drawing.Point(320, 118);
+            this.consultarButton.Name = "consultarButton";
+            this.consultarButton.Size = new System.Drawing.Size(75, 23);
+            this.consultarButton.TabIndex = 20;
+            this.consultarButton.Text = "CONSULTAR";
+            this.consultarButton.UseVisualStyleBackColor = true;
+            this.consultarButton.Click += new System.EventHandler(this.consultarButton_Click);
+            // 
+            // modinotaButton
+            // 
+            this.modinotaButton.Location = new System.Drawing.Point(410, 118);
+            this.modinotaButton.Name = "modinotaButton";
+            this.modinotaButton.Size = new System.Drawing.Size(75, 23);
+            this.modinotaButton.TabIndex = 22;
+            this.modinotaButton.Text = "MODIFICAR";
+            this.modinotaButton.UseVisualStyleBackColor = true;
+            this.modinotaButton.Click += new System.EventHandler(this.modinotaButton_Click);
+            // 
+            // form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSalmon;
+            this.ClientSize = new System.Drawing.Size(767, 400);
+            this.Controls.Add(this.modinotaButton);
+            this.Controls.Add(this.consultarButton);
+            this.Controls.Add(this.eliminarButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.notaBox);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.mhButton);
+            this.Controls.Add(this.aprobadosButton);
+            this.Controls.Add(this.suspensosButton);
+            this.Controls.Add(this.todosButton);
+            this.Controls.Add(this.agregarButton);
+            this.Controls.Add(this.notaLabel);
+            this.Controls.Add(this.nombreLabel3);
+            this.Controls.Add(this.apellidoLabel);
+            this.Controls.Add(this.dniLabel);
+            this.Controls.Add(this.nombreBox);
+            this.Controls.Add(this.apellidoBox);
+            this.Controls.Add(this.dniBox);
+            this.Name = "form";
+            this.Text = "ventana calificaiones ";
+            this.Load += new System.EventHandler(this.form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox dniBox;
+        private System.Windows.Forms.TextBox apellidoBox;
+        private System.Windows.Forms.TextBox nombreBox;
+        private System.Windows.Forms.Label dniLabel;
+        private System.Windows.Forms.Label apellidoLabel;
+        private System.Windows.Forms.Label nombreLabel3;
+        private System.Windows.Forms.Label notaLabel;
+        private System.Windows.Forms.Button agregarButton;
+        private System.Windows.Forms.Button todosButton;
+        private System.Windows.Forms.Button suspensosButton;
+        private System.Windows.Forms.Button aprobadosButton;
+        private System.Windows.Forms.Button mhButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox notaBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button eliminarButton;
+        private System.Windows.Forms.Button consultarButton;
+        private System.Windows.Forms.Button modinotaButton;
+    }
+}
+
---
+++ examen_alvarado/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace examen_alvarado
+{
+
+
+
+    public partial class form : Form
+    {
+        //lista que almacena los alumnos 
+        List<string[]> alumnos = new List<string[]>();
+        
+
+        public form()
+        {
+            InitializeComponent();
+        }
+
+        
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+           
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void agregarButton_Click(object sender, EventArgs e)
+        {
+            // Verifica si el DNI ya existe
+            bool flag = false;
+            string DNI = dniBox.Text;
+            string apellido = apellidoBox.Text;
+            string nombre = nombreBox.Text;
+
+            // Intenta convertir la nota a double
+            if (!double.TryParse(notaBox.Text, out double nota) || nota < 0 || nota > 10)
+            {
+                MessageBox.Show("Error: La nota debe ser un número válido entre 0 y 10.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Verifica si el DNI ya existe en la lista de alumnos
+            foreach (var datos in alumnos)
+            {
+                if (datos != null && datos.Length > 0 && datos[0] == DNI)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (flag)
+            {
+                MessageBox.Show("Error: El DNI ya está registrado.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                // Crea un nuevo arreglo para el alumno
+                string[] datosAlumno = new string[5];
+
+                // Llena el arreglo con los datos del alumno
+                datosAlumno[0] = DNI;
+                datosAlumno[1] = apellido;
+                datosAlumno[2] = nombre;
+                datosAlumno[3] = nota.ToString("0.00"); // Formatea la nota con dos decimales
+
+                // Calcula la calificación basada en la nota
+                if (nota < 5)
+                {
+                    datosAlumno[4] = "SS";
+                }
+                else if (nota >= 5 && nota < 7)
+                {
+                    datosAlumno[4] = "AP";
+                }
+                else if (nota >= 7 && nota < 9)
+                {
+                    datosAlumno[4] = "NT";
+                }
+                else
+                {
+                    datosAlumno[4] = "SB";
+                }
+
+                // Agrega el arreglo a la lista de alumnos
+                alumnos.Add(datosAlumno);
+
+                // Actualiza el ListBox
+                ActualizarListBox();
+            }
+        }
+
+        private void ActualizarListBox()
+        {
+            // Limpia el ListBox y agrega cada alumno de la lista
+            listBox1.Items.Clear();
+            foreach (var datos in alumnos)
+            {
+                if (datos != null && datos.Length >= 5)
+                {
+                    listBox1.Items.Add($"{datos[0]} - {datos[1]} {datos[2]} - Nota: {datos[3]} ({datos[4]})");
+                }
+            }
+        }
+
+        private void apellidoBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notaBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calificacionBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nombreBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void todosButton_Click(object sender, EventArgs e)
+        {
+            //limpio y cargo todos los alumnos en la lista al listbx haciendo un recorrido
+            listBox1.Items.Clear ();
+            foreach (var datos in alumnos)
+            {
+                string datosAlumno = $"{datos[0]} - {datos[1]} {datos[2]} - Nota: {datos[3]} ({datos[4]})";
+                listBox1.Items.Add(datosAlumno);
+            }
+        }
+
+        private void eliminarButton_Click(object sender, EventArgs e)
+        {
+            //obtengo el dni del box
+            String dni = dniBox.Text;
+            Boolean flag = false;
+            //exploro todos los vectores de la lista pero solo en la posicion 0 que pertenece al DNI
+            for (int i = 0; i < alumnos.Count; i++) {
+                if (alumnos[i][0] == dni) { 
+                    // elimino el vector perteneciente a ese dni, el i es la posicion en la lista
+                    alumnos.RemoveAt(i);
+                    flag = true;
+                    break;
+                }
+            }
+
+            //mensajes de advertencia que se envian segun el estado de la bandera
+            if(flag)
+            {
+                MessageBox.Show("Alumno eliminado", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Alumno no encontrado", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void consultarButton_Click(object sender, EventArgs e)
+        {
+            string dni = dniBox.Text;
+            bool flag = false;
+
+            // Limpia el ListBox antes de mostrar nuevos datos
+            listBox1.Items.Clear();
+
+            // Recorre la lista de alumnos
+            for (int i = 0; i < alumnos.Count; i++)
+            {
+                // Verifica si el DNI coincide
+                if (alumnos[i][0] == dni)
+                {
+                    // Muestra los datos del alumno en el ListBox
+                    listBox1.Items.Add($"DNI: {alumnos[i][0]}");
+                    listBox1.Items.Add($"Apellido: {alumnos[i][1]}");
+                    listBox1.Items.Add($"Nombre: {alumnos[i][2]}");
+                    listBox1.Items.Add($"Nota: {alumnos[i][3]}");
+                    listBox1.Items.Add($"Calificación: {alumnos[i][4]}");
+
+                    flag = true; // Marca que se encontró el alumno
+                    break;
+                }
+            }
+
+            // Si no se encontró el alumno, muestra un mensaje de advertencia
+            if (!flag)
+            {
+                MessageBox.Show("Alumno no encontrado", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void suspensosButton_Click(object sender, EventArgs e)
+        {
+         
+            listBox1.Items.Clear();
+            //exploro la lista 
+            foreach (var datos in alumnos) {
+                //busco el alumno de interes
+                if (datos[4] == "SS")
+                {
+                    
+                    string datosAlumno = $"{datos[0]} - {datos[1]} {datos[2]} - Nota: {datos[3]} ({datos[4]})";
+                    listBox1.Items.Add(datosAlumno);
+                }
+            }
+
+            if(listBox1.Items.Count == 0)
+            {
+                MessageBox.Show("Alumnos no encontrados", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+
+
+        }
+
+        private void aprobadosButton_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (var datos in alumnos)
+            {
+                if (datos[4] == "AP" || datos[4] == "NT" || datos[4] == "SB")
+                {
+                    string datosAlumno = $"{datos[0]} - {datos[1]} {datos[2]} - Nota: {datos[3]} ({datos[4]})";
+                    listBox1.Items.Add(datosAlumno);
+                }
+            }
+
+            if (listBox1.Items.Count == 0)
+            {
+                MessageBox.Show("Alumnos no encontrados", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+        }
+
+        private void mhButton_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (var datos in alumnos)
+            {
+                if (datos[3] == "10")
+                {
+                    string datosAlumno = $"{datos[0]} - {datos[1]} {datos[2]} - Nota: {datos[3]} ({datos[4]})";
+                    listBox1.Items.Add(datosAlumno);
+                }
+            }
+
+            if (listBox1.Items.Count == 0)
+            {
+                MessageBox.Show("Alumnos no encontrados", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+        }
+
+        private void modinotaButton_Click(object sender, EventArgs e)
+        {
+            String dni = dniBox.Text;
+            Double nota = Convert.ToDouble(notaBox.Text);
+            Boolean flag = false;
+            for (int i = 0; i < alumnos.Count; i++)
+            {
+                if (alumnos[i][0] == dni)
+                {
+                    alumnos[i][3] = notaBox.Text;
+                    if (nota < 5)
+                    {
+                        alumnos[i][4] = "SS";
+                    }
+                    else
+                    if (nota >= 5 && nota < 7)
+                    {
+                        alumnos[i][4] = "AP";
+                    }
+                    else
+                    if (nota >= 7 && nota < 9)
+                    {
+                        alumnos[i][4] = "NT";
+                    }
+                    else
+                    if (nota >= 9)
+                    {
+                        alumnos[i][4] = "SB";
+                    }
+                    flag = true;
+                    listBox1.Items.Clear();
+                    MessageBox.Show("Nota Modificada", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                    break;
+                }
+            }
+            if (flag == false)
+            {
+                MessageBox.Show("Alumno no encontrado", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void form_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
